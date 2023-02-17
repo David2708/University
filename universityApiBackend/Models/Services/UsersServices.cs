4eda254c15d4bcb54d49f448130e3bebf0db49a7
David <davojh27@gmail.com>
2023-02-17 17:27:24 -0500

    Se creo la clase services.cs con los metodos pedidos en el ejercicio del tema 4 ( consultas y LinQ)

---
+++ universityApiBackend/Models/Services/UsersServices.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using universityApiBackend.Models.DataModels;
+using static universityApiBackend.Models.DataModels.Course;
+
+namespace universityApiBackend.Models.Services
+{
+    public class UsersServices
+    {
+        public IEnumerable<User>? SearchUsersByEmail(List<User> listUSers, string email)
+        {
+            var usersByEMail = from user in listUSers where user.Email == email
+                               select user;
+
+            return usersByEMail;
+        }
+
+        public IEnumerable<Student>? LookForOlderStudent(List<Student> listStudents)
+        {
+            var currentyear = DateTime.Now;
+            var studentsOfLegalAge = from student in listStudents
+                               where student.DOB.Year - currentyear.Year >= 18
+                               select student;
+
+            return studentsOfLegalAge;
+        }
+
+        public IEnumerable<Student>? SearchStudentsWhoHaveAtLeastOneCourse(List<Student> listStudents)
+        {
+            var studentThanHaveMoreACourse = from student in listStudents
+                                             where student.Courses.Count() >= 1
+                                             select student;
+
+            return studentThanHaveMoreACourse;
+        }
+
+        public IEnumerable<Course>? SearchCourseByLevelThanHaveOneOrMoreStudent(List<Course> listCourses, Levels level)
+        {
+            var CousesByLevelWithMoreStudent = from course in listCourses
+                                             where course.Level == level && course.Students.Count() >= 1
+                                             select course;
+
+            return CousesByLevelWithMoreStudent;
+        }
+
+        public IEnumerable<Course>? SearchCourseByLevelAndCategory(List<Course> listCourses, Levels level, Category category)
+        {
+            var CousesByLevelAndCategegry = from course in listCourses
+                                            where course.Level == level && course.Categories == category
+                                            select course;
+
+            return CousesByLevelAndCategegry;
+        }
+
+
+    }
+}
